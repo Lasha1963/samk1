97ea09cf8558045f6915e606f0e0613748cc19dd
Lasha1963 <samkl100@gmail.com>
2026-09-25 13:06:44 +0400

    first day

--- Lesson_1/lesson_1_1.py
+++ Lesson_1/lesson_1_1.py
@@ -3,9 +3,16 @@ a=7.7 #მცურავი წერტილით
 str_1="Hi!" #მწკრივი
 # print("Veriable a is equal to ", a)
 # print(str_1)
-#
+
 # a, b, c, = 1, 2, 3
 # a,b,c=c,b,a # მოვატრიალეთ, სანამ გადაეწერება ახალი მნიშვნელობები ჯერ მიანიჭებს საწყის მნიშვნელობებს
 # print(a, b, c)
-print(type(str_1))
-print(type(a))
+
+# print(type(str_1))
+# print(type(a))
+
+message="გაუ!"
+print(message)
+print(str_1)
+print(id(message))
+print(id(str_1))
